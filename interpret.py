857d2f3ab9d9caaaa9d60488b71ed9a84857ed7a
plosnak2 <jakub.zaukolec@gmail.com>
2021-03-18 18:06:02 +0100

    pridanie 7 instrukci: add sub mul idiv lt gt eq

--- interpret.py
+++ interpret.py
@@ -42,6 +42,141 @@ def main():
         elif(instruction.opcode == 'POPS'):
             arg_type, arg_value = stack.popStack()
             variable.setTypeAndValue(instruction.arg1, arg_type, arg_value)
+        elif(instruction.opcode == 'ADD'):
+            arg_type2, arg_value2 = variable.getTypeAndValue(instruction.arg2)
+            arg_type3, arg_value3 = variable.getTypeAndValue(instruction.arg3)
+            if arg_type2 == arg_type3:
+                if arg_type2 == 'int':
+                    value = int(arg_value2) + int(arg_value3)
+                    variable.setTypeAndValue(instruction.arg1, 'int', value)
+                else:
+                    sys.stderr.write("Zly typ operandov pri instrukci ADD - musia byť int\n")
+                    sys.exit(53)
+            else:
+                sys.stderr.write("Zly typ operandov pri instrukci ADD - operandy musia mat rovnake typy INT\n")
+                sys.exit(53)
+        elif(instruction.opcode == 'SUB'):
+            arg_type2, arg_value2 = variable.getTypeAndValue(instruction.arg2)
+            arg_type3, arg_value3 = variable.getTypeAndValue(instruction.arg3)
+            if arg_type2 == arg_type3:
+                if arg_type2 == 'int':
+                    value = int(arg_value2) - int(arg_value3)
+                    variable.setTypeAndValue(instruction.arg1, 'int', value)
+                else:
+                    sys.stderr.write("Zly typ operandov pri instrukci SUB - musia byť int\n")
+                    sys.exit(53)
+            else:
+                sys.stderr.write("Zly typ operandov pri instrukci SUB - operandy musia mat rovnake typy INT\n")
+                sys.exit(53)
+        elif(instruction.opcode == 'MUL'):
+            arg_type2, arg_value2 = variable.getTypeAndValue(instruction.arg2)
+            arg_type3, arg_value3 = variable.getTypeAndValue(instruction.arg3)
+            if arg_type2 == arg_type3:
+                if arg_type2 == 'int':
+                    value = int(arg_value2) * int(arg_value3)
+                    variable.setTypeAndValue(instruction.arg1, 'int', value)
+                else:
+                    sys.stderr.write("Zly typ operandov pri instrukci MUL - musia byť int\n")
+                    sys.exit(53)
+            else:
+                sys.stderr.write("Zly typ operandov pri instrukci MUL - operandy musia mat rovnake typy INT\n")
+                sys.exit(53)
+        elif(instruction.opcode == 'IDIV'):
+            arg_type2, arg_value2 = variable.getTypeAndValue(instruction.arg2)
+            arg_type3, arg_value3 = variable.getTypeAndValue(instruction.arg3)
+            if arg_type2 == arg_type3:
+                if arg_type2 == 'int':
+                    if(int(arg_value3) == 0):
+                        sys.stderr.write("Delenie nulou.\n")
+                        sys.exit(57)
+                    else:
+                        value = int(arg_value2) // int(arg_value3)
+                        variable.setTypeAndValue(instruction.arg1, 'int', value)
+                else:
+                    sys.stderr.write("Zly typ operandov pri instrukci IDIV - musia byť int\n")
+                    sys.exit(53)
+            else:
+                sys.stderr.write("Zly typ operandov pri instrukci IDIV - operandy musia mat rovnake typy INT\n")
+                sys.exit(53)
+        elif(instruction.opcode == 'LT'):
+            arg_type2, arg_value2 = variable.getTypeAndValue(instruction.arg2)
+            arg_type3, arg_value3 = variable.getTypeAndValue(instruction.arg3)
+            if(arg_type2 != arg_type3):
+                sys.stderr.write("Zly typ operandov pri instrukci LT - operandy musia mat rovnake typy\n")
+                sys.exit(53)
+            else:
+                if(arg_type2 == 'int'):
+                    if(int(arg_value2) < int(arg_value3)):
+                        value = 'true'
+                    else:
+                        value = 'false'
+                    variable.setTypeAndValue(instruction.arg1, 'bool', value)
+                elif(arg_type2 == 'bool'):
+                    if(arg_value2 == 'false' and arg_value3 == 'true'):
+                        value = 'true'
+                    else:
+                        value = 'false'
+                    variable.setTypeAndValue(instruction.arg1, 'bool', value)
+                elif(arg_type2 == 'string'):
+                    if(arg_value2 < arg_value3):
+                        value = 'true'
+                    else:
+                        value = 'false'
+                    variable.setTypeAndValue(instruction.arg1, 'bool', value)
+                else:
+                    sys.stderr.write("Zly typ operandov pri instrukci LT - operandy možu byt intr, bool, string\n")
+                    sys.exit(53)
+        elif(instruction.opcode == 'GT'):
+            arg_type2, arg_value2 = variable.getTypeAndValue(instruction.arg2)
+            arg_type3, arg_value3 = variable.getTypeAndValue(instruction.arg3)
+            if(arg_type2 != arg_type3):
+                sys.stderr.write("Zly typ operandov pri instrukci GT - operandy musia mat rovnake typy\n")
+                sys.exit(53)
+            else:
+                if(arg_type2 == 'int'):
+                    if(int(arg_value2) > int(arg_value3)):
+                        value = 'true'
+                    else:
+                        value = 'false'
+                    variable.setTypeAndValue(instruction.arg1, 'bool', value)
+                elif(arg_type2 == 'bool'):
+                    if(arg_value2 == 'true' and arg_value3 == 'false'):
+                        value = 'true'
+                    else:
+                        value = 'false'
+                    variable.setTypeAndValue(instruction.arg1, 'bool', value)
+                elif(arg_type2 == 'string'):
+                    if(arg_value2 > arg_value3):
+                        value = 'true'
+                    else:
+                        value = 'false'
+                    variable.setTypeAndValue(instruction.arg1, 'bool', value)
+                else:
+                    sys.stderr.write("Zly typ operandov pri instrukci GT - operandy možu byt intr, bool, string\n")
+                    sys.exit(53)
+        elif(instruction.opcode == 'EQ'):
+            arg_type2, arg_value2 = variable.getTypeAndValue(instruction.arg2)
+            arg_type3, arg_value3 = variable.getTypeAndValue(instruction.arg3)
+            if(arg_type2 != 'nil' and arg_type3 != 'nil'):
+                if(arg_type2 != arg_type3):
+                    sys.stderr.write("Zly typ operandov pri instrukci EQ - operandy musia mat rovnake typy\n")
+                    sys.exit(53)
+                else:
+                    if(arg_value2 == arg_value3):
+                        value = 'true'
+                    else:
+                        value = 'false'
+                    variable.setTypeAndValue(instruction.arg1, 'bool', value)
+            else:
+                # porovnavanie s nilom je true len ked oba su nil
+                if(arg_type2 == arg_type3):
+                    if(arg_value2 == arg_value3):
+                        value = 'true'
+                    else:
+                        value = 'false'
+                    variable.setTypeAndValue(instruction.arg1, 'bool', value)
+                else:
+                    variable.setTypeAndValue(instruction.arg1, 'bool', 'false')
         
         actualInstruction += 1
         
